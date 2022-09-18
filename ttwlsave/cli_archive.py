b9eb6e2f6e3d2f7fb14c535a19630afd880721c0
CJ Kucera <cj@apocalyptech.com>
2022-09-17 21:10:51 -0500

    Okay, final version of my savegame archival script

--- ttwlsave/cli_archive.py
+++ ttwlsave/cli_archive.py
@@ -26,7 +26,7 @@ import sys
 import html
 import argparse
 import ttwlsave
-from ttwlsave.ttwlsave import TTWLSave
+from ttwlsave.ttwlsave import TTWLSave, InvSlot
 
 def main():
 
@@ -149,8 +149,8 @@ def main():
 
             # Write out the row
             print('<tr class="row{}">'.format((files_written + row_offset) % 2), file=idf)
+            print(f'<td class="filename"><a href="https://github.com/apocalyptech/blsaves/raw/main/wl/{base_filename}">{base_filename}</a></td>', file=idf)
             print(f'<td class="level">{char_level}</td>', file=idf)
-            print(f'<td class="filename"><a href="wl/{base_filename}">{base_filename}</a></td>', file=idf)
             print('<td class="in_map">{}</td>'.format(save.get_pt_last_map(0, True)), file=idf)
             missions = save.get_pt_active_mission_list(0, True)
             if len(missions) == 0:
@@ -167,7 +167,7 @@ def main():
         # May as well force the name, while we're at it
         save.set_char_name("WL Savegame Archive")
 
-        # Randomize GUID
+        # Hardcode GUID
         save.randomize_guid()
 
         # Max XP
@@ -185,8 +185,8 @@ def main():
 
         # Currency
         save.set_money(50000000) # Fifty million
-        save.set_moon_orbs(10000)
-        save.set_souls(200)
+        save.set_moon_orbs(16000)
+        save.set_souls(1400)
 
         # Max SDUs
         save.set_max_sdus()
@@ -195,7 +195,15 @@ def main():
         save.set_max_ammo()
 
         # Unlock all inventory slots
-        save.unlock_slots()
+        save.unlock_slots([
+            # Not unlocking the second spell slot since that's class-specific
+            InvSlot.WEAPON3,
+            InvSlot.WEAPON4,
+            InvSlot.ARMOR,
+            InvSlot.RING1,
+            InvSlot.RING2,
+            InvSlot.AMULET,
+            ])
 
         # Unlock Feats/Companions
         # (Not gonna do this after all, I don't think.)
@@ -227,6 +235,83 @@ def main():
             # The serials above don't have any rerolls logged, but we may as well
             # be sure about that anyway
             item.rerolled = 0
+
+        # Make sure all saves have my save-archive customizations selected
+        # (this is, generally, the case already, except that the Banner and
+        # Statue customizations can't get chosen until either Brighthoof or
+        # Dreamveil Overlook).
+        save.save.selected_customizations[:] = [
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/SkinTone/SkinToneColor_21.SkinToneColor_21',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Head/HeadType_11.HeadType_11',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/HairColor/HairColor_09.HairColor_09',
+                '/Game/PatchDLC/Indigo4/PlayerCharacters/_Shared/_Design/Customization/HeadAccessories/HeadAccessory_55.HeadAccessory_55',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/EyeShape/EyeShape_03.EyeShape_03',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Eyebrows/Eyebrows_05.Eyebrows_05',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/PupilShape/Pupil_15.Pupil_15',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/EyeColor/EyeColor_03.EyeColor_03',
+                '/Game/PatchDLC/Indigo2/PlayerCharacters/_Shared/_Design/EarShapes/EarShape_16.EarShape_16',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/NoseShape/NoseShape_06.NoseShape_06',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/MouthShape/MouthShape_09.MouthShape_09',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Scar/Scars_00.Scars_00',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Tattoo/Tattoos_00.Tattoos_00',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/TattooColor/TattooColor_01.TattooColor_01',
+                '/Game/PatchDLC/Indigo3/PlayerCharacters/_Shared/Design/Customization/FaceAccessories/FaceAccessory_29.FaceAccessory_29',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Makeup/Blush/BlushShape_00.BlushShape_00',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Makeup/BlushColor/MakeupColor_01_Blush.MakeupColor_01_Blush',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Makeup/EyeLiner/EyelinerShape_00.EyelinerShape_00',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Makeup/EyeLinerColor/MakeupColor_01_Eyeliner.MakeupColor_01_Eyeliner',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Makeup/EyeShadow/EyeShadow_00.EyeShadow_00',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Makeup/EyeShadowColor/MakeupColor_01_Eyeshadow.MakeupColor_01_Eyeshadow',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Makeup/Lipstick/Lipstick_00.Lipstick_00',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Makeup/LipstickColor/MakeupColor_01_Lipstick.MakeupColor_01_Lipstick',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Body/BodyShape_02.BodyShape_02',
+                '/Game/PatchDLC/Indigo2/PlayerCharacters/_Shared/_Design/ArmorColor/ArmorColor_80_Primary.ArmorColor_80_Primary',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Armor/ColorSecondary/ArmorColor_69_Secondary.ArmorColor_69_Secondary',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Armor/ColorTertiary/ArmorColor_48_Tertiary.ArmorColor_48_Tertiary',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Armor/UnderPattern/UnderArmorPattern_22.UnderArmorPattern_22',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Emotes/Emote_09.Emote_09',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Emotes/Emote_11.Emote_11',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Armor/Pattern/ArmorPattern_02.ArmorPattern_02',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Eyelashes/Eyelashes_02.Eyelashes_02',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Scar/ScarFlip_00.ScarFlip_00',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Tattoo/TattooFlip_00.TattooFlip_00',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Banner/Pattern/BannerPattern_04.BannerPattern_04',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Banner/PatternColor/BannerColor_27_Pattern.BannerColor_27_Pattern',
+                '/Game/PatchDLC/Indigo2/PlayerCharacters/_Shared/_Design/Banners/Banner_Shape_23.Banner_Shape_23',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Banner/BackgroundColor/BannerColor_30_Shape.BannerColor_30_Shape',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Banner/Icon/BannerIcon_12.BannerIcon_12',
+                '/Game/PatchDLC/Indigo4/PlayerCharacters/_Shared/_Design/Customization/Statues/Poses/HeroStatuePose_28.HeroStatuePose_28',
+                '/Game/PatchDLC/Indigo4/PlayerCharacters/_Shared/_Design/Customization/Statues/Materials/HeroStatueMaterial_25.HeroStatueMaterial_25',
+                '/Game/PlayerCharacters/_Shared/_Design/Customization/Banner/IconColor/BannerColor_29_Icon.BannerColor_29_Icon',
+                ]
+
+        # And I suppose if I'm enforcing *those* customizations, I may as well get the sliders, too,
+        # as well as voice + pronoun stuff.  Do it right!
+        sliders = {
+                'LeftEyePosX': 0.012512505,
+                'RightEyePosX': -0.012512505,
+                'LeftEyePosY': -0.13783783,
+                'RightEyePosY': -0.13783783,
+                'LeftEyeRot': -0.46546555,
+                'RightEyeRot': 0.46546555,
+                'LeftEyeScale': 0.9592593,
+                'RightEyeScale': 0.9592593,
+                'LeftEarScale': 1.0660659,
+                'RightEarScale': 1.0660659,
+                'NosePosY': -0.2181181,
+                'NoseScale': 0.88588595,
+                'MouthPosY': 0.3068068,
+                'MouthUpperLipScale': 1.1223223,
+                'MouthLowerLipScale': 1.0810812,
+                'BodyScale': 1.0001802,
+                'HeadAndNeck_Scale': 1.0009409,
+                }
+        for c in save.save.custom_float_customizations:
+            if c.name in sliders:
+                c.value = sliders[c.name]
+        save.save.player_voice.data = '/Game/Dialog/Nametags/DNT_PlBraveF.DNT_PlBraveF'
+        save.save.player_voice.pitch = 0.5
+        save.save.player_pronoun_selection = '/Game/PlayerCharacters/_Shared/_Design/PlayerPronouns/PlayerPronouns_Neutral.PlayerPronouns_Neutral'
 
         # Write out
         save.save_to(output_filename)
